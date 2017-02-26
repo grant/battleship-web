10c0c907686539757fe0332ab27c4ae7ec2133b2
Grant Timmerman <granttimmerman@gmail.com>
2017-02-26 11:57:35 -0800

    Add modal

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,38 @@ import logo from '../images/logo.svg';
 import './App.css';
 
 class App extends Component {
+  nameInput = '';
+
+  /**
+   * The app's intro modal.
+   */
+  getModal() {
+    return (
+      <div className="modal">
+        <h1>Battleship</h1>
+        <h3>Ship conquering game</h3>
+        <input ref={(input) => {this.nameInput = input;}} type="text" name="name" placeholder="Empire Name"/>
+        <button onClick={this.play.bind(this)}>Play</button>
+        <hr/>
+        <ul>
+          <li><a href="">Controls</a></li>
+          <li><a href="">Reddit</a></li>
+        </ul>
+        <footer>Game by <a href="https://grant.cm">Grant Timmerman</a></footer>
+      </div>
+    );
+  }
+
+  play() {
+    console.log(this.nameInput.value);
+    console.log(this.refs.name);
+  }
+
   render() {
     // <img src={logo} className="App-logo" alt="logo" />
     return (
       <div className="App">
-        <div className="modal">
-          <h1>Battleship</h1>
-          <h3>Ship conquering game</h3>
-          <input type="text" name="name" placeholder="Empire Name"/>
-          <button>Play</button>
-          <hr/>
-          <ul>
-            <li><a href="">Controls</a></li>
-            <li><a href="">Reddit</a></li>
-          </ul>
-          <footer>Game by <a href="https://grant.cm">Grant Timmerman</a></footer>
-        </div>
+        {this.getModal()}
         <div className="map"></div>
       </div>
     );
